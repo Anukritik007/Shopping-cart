a7acd0f614936c1e1662604e269b43f100f672c3
Anukritik007 <anukritik77@gmail.com>
2020-09-29 21:31:47 +0530

    addded tab navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'shopping-list';
+  public activePage: string ='recipes';
+
+  public onHeaderItemClick(item: string) {
+    this.activePage = item;
+  }
 }
